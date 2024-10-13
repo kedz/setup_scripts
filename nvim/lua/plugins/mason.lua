54a52bab00553a71e9bdfb5d5a674fda03bfb4fd
kedz <kedzie@cs.columbia.edu>
2024-10-12 23:29:28 -0400

    Add formatters and auto install of formatters, linters.

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,6 +1,32 @@
 return {
 	"williamboman/mason.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
 	config = function()
-		require("mason").setup()
+		local mason = require("mason")
+
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"ruff", -- python formatter
+				"pyright",
+			},
+		})
 	end,
 }
